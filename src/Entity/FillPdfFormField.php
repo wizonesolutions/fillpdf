50309bac993f35325700ef6a611785fe18058d79
Kevin Kaland <kevin@wizonesolutions.com>
2015-04-13 15:31:04 +0200

    Sketch out FillPdfFormField entity form.

--- src/Entity/FillPdfFormField.php
+++ src/Entity/FillPdfFormField.php
@@ -19,6 +19,12 @@ use Drupal\fillpdf\FillPdfFormFieldInterface;
  * @ContentEntityType(
  *   id = "fillpdf_form_field",
  *   label = @Translation("FillPDF form field"),
+ *   handlers = {
+ *     "views_data" = "Drupal\fillpdf\FillPdfFormFieldViewsData",
+ *     "form" = {
+ *       "edit" = "Drupal\fillpdf\Form\FillPdfFormFieldForm",
+ *     },
+ *   },
  *   admin_permission = "administer pdfs",
  *   base_table = "fillpdf_fields",
  *   data_table = "fillpdf_fields_field_data",
@@ -32,6 +38,7 @@ class FillPdfFormField extends ContentEntityBase implements FillPdfFormFieldInte
 
   /**
    * @inheritdoc
+   * @todo Fix field descriptions to match D7 version.
    */
   public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
     $fields = array();
@@ -57,26 +64,41 @@ class FillPdfFormField extends ContentEntityBase implements FillPdfFormFieldInte
       ->setDescription(t('The name of the field in the PDF form.'));
 
     $fields['label'] = BaseFieldDefinition::create('string')
-      ->setLabel(t('PDF Field Label'))
-      ->setDescription(t('A reminder to assist in remembering to which field a PDF key corresponds.'));
+      ->setLabel(t('PDF field label'))
+      ->setDescription(t('An optional label to help you identify the field.'))
+      ->setDisplayOptions('form', array(
+        'type' => 'string',
+      ));
 
     $fields['prefix'] = BaseFieldDefinition::create('string_long')
       ->setLabel(t('Text before field value'))
-      ->setDescription(t('Text to add to the front of the field value unless the field value is blank.'));
+      ->setDescription(t('Text to add to the front of the field value unless the field value is blank.'))
+      ->setDisplayOptions('form', array(
+        'type' => 'string_long',
+      ));
 
+    // @todo: convert meeeeeeee
     $fields['value'] = BaseFieldDefinition::create('string_long')
       ->setLabel(t('Fill pattern'))
-      ->setDescription(t('Text and tokens with which to fill in the PDF.'));
+      ->setDescription(t('Text and tokens with which to fill in the PDF.'))
+      ->setDisplayOptions('form', array(
+        'type' => 'string_long',
+      ));
 
     $fields['suffix'] = BaseFieldDefinition::create('string_long')
       ->setLabel(t('Text after field value'))
-      ->setDescription(t('Text to add to the end of the field value unless the field value is blank.'));
+      ->setDescription(t('Text to add to the end of the field value unless the field value is blank.'))
+      ->setDisplayOptions('form', array(
+        'type' => 'string_long',
+      ));
 
     // @todo: Can I do better on field type here? Maybe a multi-value string field?
-    // replacements
     $fields['replacements'] = BaseFieldDefinition::create('string_long')
       ->setLabel(t('Fill pattern transformations'))
-      ->setDescription(t('Pipe-separated mapping of specific values to replace with other values.'));
+      ->setDescription(t('Pipe-separated mapping of specific values to replace with other values.'))
+      ->setDisplayOptions('form', array(
+        'type' => 'string_long',
+      ));
 
     return $fields;
   }
